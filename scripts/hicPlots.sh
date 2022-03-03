51ded3ed39650ecec6ecabbabe7ef53c9c30f907
cjr41 <croth@pn2034279.lanl.gov>
2022-03-03 08:45:16 -0700

    Added h5 and hic example data.

--- scripts/hicPlots.sh
+++ scripts/hicPlots.sh
@@ -1,11 +1,13 @@
-## List of commands used to plot hic-matrices
+##
+## List of hicexplorer commands used to plot hic-matrices
 ## by - Cullen Roth
 ## 
 ## NOTE: be sure to activate python environment with 
 ##       an installation of hicExplorer. for e.g.
 ## 
-## conda activate pygenometracks
+conda activate pygenometracks
 ## 
+##
 ## For chromosome 6
 hicPlotMatrix --matrix ../data/ENCLB571GEP.chr6.200kb.h5 \
     --title "A549 Chromosome 6 Hi-C Contacts" \
@@ -20,5 +22,6 @@ hicPlotMatrix --matrix ../data/ENCLB571GEP.chr22.200kb.h5 \
     --rotationX 90 --log1p --dpi 300 --colorMap coolwarm \
     --increaseFigureHeight 1.25 --increaseFigureWidth 1.25 --fontsize 6 \
     --outFileName ../figures/ENCLB571GEP.chr22.png 
-
+##
 ## 
+## fin
