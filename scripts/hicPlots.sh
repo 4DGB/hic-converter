ec212c19735f755c4f9a73701b346b1796ae6e5d
cjr41 <croth@pn2034279.lanl.gov>
2022-03-14 09:57:20 -0600

    Updated plotting scripts.

--- scripts/hicPlots.sh
+++ scripts/hicPlots.sh
@@ -1,23 +1,19 @@
-##
+#!/bin/zsh
+
 ## List of hicexplorer commands used to plot hic-matrices
-## by - Cullen Roth
-## 
-## NOTE: be sure to activate python environment with 
-##       an installation of hicExplorer. for e.g.
-## 
+## by: Cullen Roth
+
+## Sourced user's zsh file
+source ~/.zshrc
+
+## Activate hicenvironment env
 conda activate hicexplorerenv
-## 
-##
-## For chromosome 6
-hicPlotMatrix --matrix ../data/h5/ENCLB571GEP.chr6.200kb.h5 --outFileName ../data/figures/ENCLB571GEP.chr6.png \
-    --title "A549 Chromosome 6 Hi-C Contacts" --rotationX 90 --log1p --dpi 300 \
-    --colorMap coolwarm --increaseFigureHeight 1.25 --increaseFigureWidth 1.25 --fontsize 6 
-## 
-## 
-## For chromosome 22
-hicPlotMatrix --matrix ../data/h5/ENCLB571GEP.chr22.200kb.h5 --outFileName ../data/figures/ENCLB571GEP.chr22.png \
-    --title "A549 Chromosome 22 Hi-C Contacts" --rotationX 90 --log1p --dpi 300 \
-    --colorMap coolwarm --increaseFigureHeight 1.25 --increaseFigureWidth 1.25 --fontsize 6 
-##
-## 
-## fin
+
+## For hic data (in h5 format) taken from hour 00 and 12 of dexamethason exsposure 
+## and for chromosomes 6 and 22 plot hic matrix 
+for sample in ENCLB571GEP.chr22.200kb.00.h5 ENCLB870JCZ.chr22.200kb.12.h5 ENCLB571GEP.chr6.200kb.00.h5 ENCLB870JCZ.chr6.200kb.12.h5
+do
+    hicPlotMatrix --matrix ../data/h5/${sample} --outFileName ../figures/${sample}.png \
+                  --rotationX 90 --log1p --dpi 150 --colorMap coolwarm \
+                  --increaseFigureHeight 1.25 --increaseFigureWidth 1.25 --fontsize 10
+done
